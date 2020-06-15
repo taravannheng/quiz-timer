ae708f4760c222a20bc76e4a0be884ff59ee422f
slick <62088941+taravannheng@users.noreply.github.com>
2020-06-16 08:02:18 +1000

    Update time displayer color
    Change the color of the time displayer in regards to each phase per question

--- script/script.js
+++ script/script.js
@@ -35,6 +35,7 @@ $(document).ready(function() {
         //updates question displayer
         $('#question-displayer').css('color', '#606060');
         $('#left-display-details p').css('color', '#606060');
+        $('#time-displayer').css('color', '#A9DC76');
 
         var i = 0;
     
@@ -57,6 +58,9 @@ $(document).ready(function() {
                 $('#question-displayer').css('color', '#A9A9A9');
                 $('#left-display-details p').css('color', '#A9A9A9');
 
+                //reset time displayer color to default
+                $('#time-displayer').css('color', '#A9A9A9');
+
                 //enable start button
                 $('#condition-start-button').prop('disabled', false);
 
@@ -68,16 +72,18 @@ $(document).ready(function() {
 
                 //tTimer section
                 var j = 1;
+                var timePerPhase = timePerQ / 3;
 
                 var tTimer = setInterval(tCountdown, 1000);
 
                 function tCountdown() {
-                    
-
                     if (j == timePerQ) {
                         if (i <= noQuestion) {
-                            //reset the timer to 60
+                            //reset the timer
                             $('#time-displayer').text(timePerQ);
+
+                            //reset the color to first phase
+                            $('#time-displayer').css('color', '#A9DC76');
                         } else {
                             $('#time-displayer').text(0);
                         }
@@ -85,7 +91,29 @@ $(document).ready(function() {
                         //stop the timer
                         clearInterval(tTimer);
                     } else {
+                        //displayer time availble per question
                         $('#time-displayer').text(timePerQ - j);
+
+                        //change color for each phase
+                        if ((timePerQ - j) > (timePerPhase * 2)) {
+                            $('#time-displayer').css('color', '#A9DC76');
+                        }
+                        else if ((timePerQ - j) > (timePerPhase) && (timePerQ - j) < (timePerPhase * 2)) {
+                            $('#time-displayer').css('color', '#FFD866');
+                        }
+                        else {
+                            $('#time-displayer').css('color', '#FF6188');
+                        }
+                        
+
+                        /*Note: change the above color to 
+                                #A9DC76 first phase     green
+                                #FFD866 second phase    yellow
+                                #FF6188 third phase     pink
+                                once conditions are filled*/
+
+
+                        //increment
                         j++;
                     }
                 }
